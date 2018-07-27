d753c65a689bce82ba3ec069149acb7d7d4e2cdd
Penciler <eraser41@livemail.tw>
2018-07-27 09:05:07 +0800

    add post show page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,15 @@
 class PostsController < ApplicationController
-before_action :authenticate_user!, :only =>:destroy	
+before_action :authenticate_user!, only: [:destroy,:show]	
 before_action :authenticate_admin, :only =>:destroy	
 
 	def index
 		@posts=Post.page(params[:page]).per(20)
+	end
+
+	def show
+		@post=Post.find(params[:id])
+		@user=User.find(@post.user_id)
+		@replies=@post.replies
 	end
 
 	def destroy
